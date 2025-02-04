62cccca893490a88d47dbad0635985bd72622da6
ElNotsniw <winstonjosh010604@gmail.com>
2025-02-04 11:09:59 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -3,7 +3,7 @@
 // Declaring the variable, "customer" and giving it three properties: Name, age, and email
 
 
-let customer = {       
+const customer = {       
     name: "John Doe",
     age: 35,
     email: "john.doe@email.com"
@@ -13,6 +13,34 @@ let customer = {
 // Console-logging each property of the variable "customer" using a template literal
 
 
-console.log(`Name: ${customer.name}`)
-console.log(`Age: ${customer.age}`)
-console.log(`Email: ${customer.email}`)
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
+
+
+
+// Task 2 - Object Methods
+
+// Declaring the variable, "order" to have three properties: OrderID, totalamount, and status
+
+
+const order = {
+    orderID: 12345,
+    totalAmount: 150,
+    status: "Processing",
+
+
+    // Using the displayorder method and console-logging the order details from the variable
+
+    displayOrder: function() {
+        console.log (`OrderID: ${this.orderID}`);
+        console.log(`Total Amount: $${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }
+};
+
+
+// Calling back the variable
+
+order.displayOrder();
+
