f4f25ea2ed292cb6e80229ce2d854331447bdd46
ElNotsniw <winstonjosh010604@gmail.com>
2025-02-04 10:58:10 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,18 @@
+// Task 1 - Object Properties
+
+// Declaring the variable, "customer" and giving it three properties: Name, age, and email
+
+
+let customer = {       
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+
+// Console-logging each property of the variable "customer" using a template literal
+
+
+console.log(`Name: ${customer.name}`)
+console.log(`Age: ${customer.age}`)
+console.log(`Email: ${customer.email}`)
